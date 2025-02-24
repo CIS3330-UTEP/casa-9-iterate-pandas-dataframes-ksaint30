c64c0e6c39af5bae7f5db2b8302fdcbf00e1ca87
ksaint30 <san.kimtx@gmail.com>
2025-02-24 02:11:00 -0700

    Try 1st

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,14 @@
+import pandas as pd
+df = pd.read_csv("big-mac-full-index.csv")
+# iterrow()
+for index, row in df.iterrows():
+    total_price =row['local_price']*row['dollar_ex']
+    print(f"{row['name']} ${total_price}")
 
+# apply()
+def calculate_local_price(row):
+    return f"{row['name']} - Local Price: {row['local_price']}"
+
+results = df.apply(calculate_local_price, axis=1)
+for res in results.head(6):
+    print(res)
